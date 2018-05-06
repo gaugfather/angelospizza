07399107b453b6a186db2708a93b2bc3affacf00
gaugfather <derek.gauger@gmail.com>
2018-05-05 21:57:54 -0500

    initial version

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Button } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 import {Link} from "react-router-dom";
 import './header.css';
 import facebookIcon from "./facebook_icon.png";
