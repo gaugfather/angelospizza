363abfd24983dc5a5f37e995347c3c461fa85356
gaugfather <derek.gauger@gmail.com>
2018-08-28 11:37:39 -0500

    fix build and deploy

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import {BrowserRouter, Route} from "react-router-dom";
 import App from './components/app/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
     <Route component={App}/>
   </BrowserRouter>, document.getElementById('root')
 );
-
-registerServiceWorker();
