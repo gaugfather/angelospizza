5b669bf2ae24ad87c6ba2d8302d1c019ea06d9bf
gaugfather <derek.gauger@gmail.com>
2018-12-29 16:25:30 -0600

    update modules

--- src/components/header/openingModal/OpeningModal.js
+++ src/components/header/openingModal/OpeningModal.js
@@ -21,10 +21,10 @@ class OpeningModal extends Component {
 
     return (
       <Modal dimmer={'inverted'} open={this.state.isOpen} onClose={this.close}>
-        <Modal.Header><p className="welcome1"><Icon name='newspaper outline' style={{marginRight: '12px'}}/>Today's News!</p></Modal.Header>
+        <Modal.Header><p className="welcome1"><Icon name="newspaper" style={{marginRight: '12px'}}/>Today's News!</p></Modal.Header>
         <Modal.Content>
           <Modal.Description>
-            <p className="welcome2"><Icon name='search' />We're hiring Part-time Bartenders! <br />   <span style={{marginLeft: '1em'}}>Apply within! </span><Icon name='beer' style={{marginLeft: '6px'}}/><Icon name='beer' style={{marginLeft: '6px'}}/></p>            
+            <p className="welcome2"><Icon name='search' />We're hiring Part-time Bartenders! <br />   <span style={{marginLeft: '1em'}}>Apply within! </span><Icon name='beer' style={{marginLeft: '6px'}}/><Icon name='beer' style={{marginLeft: '6px'}}/></p>
           </Modal.Description>
         </Modal.Content>
         <Modal.Actions>
