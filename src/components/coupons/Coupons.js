bc23c1122330bd6175e7a2bb3b2777833b052214
gaugfather <derek.gauger@gmail.com>
2020-09-30 07:37:03 -0500

    oct coupons

--- src/components/coupons/Coupons.js
+++ src/components/coupons/Coupons.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import coupons from "../coupons/coupons0920.png";
+import coupons from "../coupons/coupons1020.png";
 import './coupons.css';
 
 class Coupons extends Component {
